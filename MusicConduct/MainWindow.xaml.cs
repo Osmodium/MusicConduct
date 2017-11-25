8a7e489fa902015de143001e5d5149e95f023c72
Christian Schubert <schubertchr@gmail.com>
2017-11-25 04:22:24 +0100

    Added dynamic notification icon to the tray depending on the icon selected in the properties of the project. Minor changes too.

--- MusicConduct/MainWindow.xaml.cs
+++ MusicConduct/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.Windows;
+﻿using System;
+using System.Drawing;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 using MusicConduct.Controls;
+using MusicConduct.Utility;
+using Brush = System.Windows.Media.Brush;
+using Color = System.Windows.Media.Color;
 
 namespace MusicConduct
 {
@@ -9,12 +14,13 @@ namespace MusicConduct
     {
         private readonly Brush m_LinkLabelForegroundDefault = new SolidColorBrush(Color.FromRgb(255, 255, 255));
         private readonly Brush m_LinkLabelForegroundHighlight = new SolidColorBrush(Color.FromRgb(35, 255, 200));
-
-        private Controls.AboutControl m_AboutControl = null;
+        private AboutControl m_AboutControl;
+        private Icon m_Icon = null;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeNotificationIcon();
 
             AboutLinkLabel.MouseEnter += LinkLabel_MouseEnter;
             AboutLinkLabel.MouseLeave += LinkLabel_MouseLeave;
@@ -27,6 +33,31 @@ namespace MusicConduct
             TitleBarMover.MouseDown += TitleBarDockOnMouseDown;
         }
 
+        private void InitializeNotificationIcon()
+        {
+            m_Icon = WindowsHelper.GetAppIcon(this);
+            if (m_Icon == null)
+                return;
+
+            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon
+            {
+                Icon = m_Icon,
+                Visible = true
+            };
+            ni.DoubleClick += delegate
+            {
+                Show();
+                WindowState = WindowState.Normal;
+            };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+            base.OnStateChanged(e);
+        }
+
         private void TitleBarDockOnMouseDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
             if (mouseButtonEventArgs.ChangedButton == MouseButton.Left)
@@ -35,7 +66,9 @@ namespace MusicConduct
 
         private void MinimizeLinkLabelOnMouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            MessageBox.Show("Minimize");
+            if (m_Icon == null)
+                return;
+            WindowState = WindowState.Minimized;
         }
 
         private void AboutLinkLabelOnMouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
